90cabebb35bbedc78f5c920f17e6629af5fa2b76
GlorYouth <admin@gloryouth.com>
2025-03-04 19:33:43 +0800

    add optional download url

--- src/main.rs
+++ src/main.rs
@@ -162,7 +162,7 @@ fn main() {
     rt.block_on(async {
         let path = "output.mp4";
         ParallelDownloader::new(
-            "http://192.168.2.3:5244/d/outer/local/output.mp4".into(),
+            "http://192.168.2.3:5244/d/outer/local/output.mp4".into(), // https://alist.gloryouth.com/d/outer/edu/%E6%88%90%E7%89%87/%E8%8A%82%E7%9B%AE1_%E6%94%B9_ai.mp4
             path.into(),
         )
         .start(20)
